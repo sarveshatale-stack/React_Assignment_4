f1ede9d19185f208f52f11c68cbab09f2e099870
sarveshatale-stack <71748819+sarveshatale-stack@users.noreply.github.com>
2020-10-02 22:02:53 +0530

    Date display on toggle

--- src/components/DigitalClock.js
+++ src/components/DigitalClock.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-
+import "./DigitalClock.css";
 class DigitalClock extends Component {
   constructor() {
     super();
-    this.state = { time: new Date() };
+    this.state = { time: new Date(), checked: false };
   }
+  handleChange = () => {
+    this.setState({ checked: !this.state.checked });
+  };
 
   componentDidMount() {
     this.update = setInterval(() => {
@@ -17,12 +20,45 @@ class DigitalClock extends Component {
   }
 
   render() {
-    const { time } = this.state;
-
+    const { time, displaydate } = this.state;
+    let date;
+    if (this.state.checked) {
+      date = <h2>{time.toLocaleDateString()}</h2>;
+    } else {
+      date = "";
+    }
+    // let date;
+    // if (displaydate) {
+    //   date = <h2>{time.toLocaleDateString()}</h2>
+    // }
+    // else
+    // {
+    //   date = ""
+    // }
     return (
       <div>
-        <h1>Digital Clock</h1>
-        <h2>{time.toLocaleTimeString()}</h2>
+        <div className="digitaldiv">
+          <div className="digitalheaderdiv">
+            <div>
+              <img src="./images/image1.jpg" />
+              Digital Clock
+            </div>
+          </div>
+          <br />
+          <div>
+            <label className="switch">
+              <input
+                type="checkbox"
+                onClick={this.handleChange}
+                defaultChecked={this.state.checked}
+              />
+              <span class="slider round"></span>
+            </label>
+            <img src="./images/image2.jpg" />{" "}
+          </div>
+          {date}
+          <h2>{time.toLocaleTimeString()}</h2>
+        </div>
       </div>
     );
   }
