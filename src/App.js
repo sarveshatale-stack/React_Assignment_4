b86812f5466b3ca45675dc04f6c375c2963a1446
sarveshatale-stack <71748819+sarveshatale-stack@users.noreply.github.com>
2020-10-02 20:07:39 +0530

    Degital Clock

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./styles.css";
 import Calculator from "./components/Calculator";
 import "./components/Calculator.css";
+import DigitalClock from "./components/DigitalClock";
 export default function App() {
   return (
     <div className="App">
-      <Calculator />
+      {/* <Calculator /> */}
+      <DigitalClock />
       {/* <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2> */}
     </div>
